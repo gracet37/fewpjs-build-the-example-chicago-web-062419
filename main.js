2118a4fda5b0fe69fcfe665f045d3a13b44cf7d3
github username <github email address>
2019-08-12 21:04:20 -0500

    Done.

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
+// window.addEventListener('DOMContentLoaded', (event) => {
+//   console.log('DOM fully loaded and parsed');
+// });
 // Defining text characters for the empty and full hearts for you to use later.
+
+
+// Your JavaScript code goes here!
+
+let body = document.querySelector('body')
+let getHearts = document.getElementsByClassName('like-glyph') // returns an HTML collection
+
+let getArray = Array.from(getHearts) //turn the HTML collection into array object
+
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+getArray.map(heart => { 
+  heart.addEventListener("click", (event) => {
+    if (event.target.innerHTML === `${EMPTY_HEART}`) {
+      return event.target.innerHTML = `${FULL_HEART}`
+    } else {
+      return event.target.innerHTML = `${EMPTY_HEART}`
+    }
+})
+})
+
 
 
 
@@ -11,15 +32,15 @@ const FULL_HEART = '♥'
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
-  });
-}
+// function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+//   return new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//       let isRandomFailure = Math.random() < .2
+//       if (isRandomFailure) {
+//         reject("Random server error. Try again.");
+//       } else {
+//         resolve("Pretend remote server notified of action!");
+//       }
+//     }, 300);
+//   });
+// }
